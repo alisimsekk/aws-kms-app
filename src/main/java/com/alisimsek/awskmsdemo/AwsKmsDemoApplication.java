922393bc4015ef55f0e3f3ae1814aa4d35948925
Ali Simsek <ali.simsek035@gmail.com>
2025-06-15 16:13:55 +0300

    Initial commit

---
+++ src/main/java/com/alisimsek/awskmsdemo/AwsKmsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.alisimsek.awskmsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsKmsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsKmsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alisimsek/awskmsdemo/AwsKmsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alisimsek.awskmsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsKmsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
